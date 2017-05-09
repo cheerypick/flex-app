f763465ff89b04a88aa3806f43374cede7ca4f6a
Katrine Orlova <katrineorlova@gmail.com>
2017-05-09 10:02:51 -0500

    Add media query observable

--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import {AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database';
+import {Component, OnInit, OnDestroy} from "@angular/core";
+import {AngularFireDatabase, FirebaseListObservable} from "angularfire2/database";
+import {Subscription} from "rxjs/Subscription";
+import {MediaChange, ObservableMedia} from "@angular/flex-layout";
 
 
 @Component({
@@ -7,12 +9,28 @@ import {AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database
   templateUrl: './pokemon-list.component.html',
   styleUrls: ['./pokemon-list.component.css']
 })
-export class PokemonListComponent implements OnInit {
+export class PokemonListComponent implements OnInit, OnDestroy {
 
+  watcher: Subscription;
+  activeMediaQuery = "";
   items: FirebaseListObservable<any[]>;
-  constructor(db: AngularFireDatabase) {
+
+  constructor(db: AngularFireDatabase, media: ObservableMedia) {
     this.items = db.list('/pokemon');
     console.log(this.items);
+
+    this.watcher = media.subscribe((change: MediaChange) => {
+      this.activeMediaQuery = change ? `'${change.mqAlias}' = (${change.mediaQuery})` : "";
+      console.log(change);
+      if (change.mqAlias == 'xs') {
+        console.log('mobile');
+      }
+    });
+
+  }
+
+  ngOnDestroy() {
+    this.watcher.unsubscribe();
   }
 
   ngOnInit() {
